3656b891befd9b789a7e2a99bc073c3a6ce05de6
louisefindlay23 <louisefindlay23@outlook.com>
2019-11-01 15:18:52 +0000

    Linked the btnAppName button to the MainActivity for the Show and Recommendations activities to act as a help button.

--- app/src/main/java/uk/ac/rgu/rectv/RecommendationsActivity.java
+++ app/src/main/java/uk/ac/rgu/rectv/RecommendationsActivity.java
@@ -3,9 +3,9 @@ package uk.ac.rgu.rectv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class RecommendationsActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,6 +14,12 @@ public class RecommendationsActivity extends AppCompatActivity implements View.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommendations);
+
+        // get the AppName button
+        Button btnAppName = findViewById(R.id.btnAppName);
+
+        // set the click listener to the btnAppName burtton
+        btnAppName.setOnClickListener(this);
 
         // get the PersonofInterest Poster image
         ImageView ivPersonofInterestPoster = findViewById(R.id.ivPersonofInterestPoster);
@@ -29,6 +35,13 @@ public class RecommendationsActivity extends AppCompatActivity implements View.O
         if (view.getId() == R.id.ivPersonofInterestPoster) {
             // create an intent
             Intent intent = new Intent(getApplicationContext(), Show.class);
+            // start the Activity
+            startActivity(intent);
+        }
+
+        else if (view.getId() == R.id.btnAppName) {
+            // create an intent
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             // start the Activity
             startActivity(intent);
         }
--- app/src/main/java/uk/ac/rgu/rectv/Show.java
+++ app/src/main/java/uk/ac/rgu/rectv/Show.java
@@ -2,13 +2,34 @@ package uk.ac.rgu.rectv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Show extends AppCompatActivity {
+public class Show extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show);
+
+        // get the AppName button
+        Button btnAppName = findViewById(R.id.btnAppName);
+
+        // set the click listener to the btnAppName burtton
+        btnAppName.setOnClickListener(this);
+    }
+
+    // Changing Activity
+    @Override
+    public void onClick(View view) {
+        // view is the View (Button, ExitText, TextView, etc) that was clicked
+        if (view.getId() == R.id.btnAppName) {
+            // create an intent
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            // start the Activity
+            startActivity(intent);
+        }
     }
 }
