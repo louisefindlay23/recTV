5afc94e118103f075eae5064b088a587d42276d1
louisefindlay23 <louisefindlay23@outlook.com>
2019-11-01 14:55:50 +0000

    Linked the MainActivity to the RecommendationsActivity by clicking the btnContinue button.

--- app/src/main/java/uk/ac/rgu/rectv/MainActivity.java
+++ app/src/main/java/uk/ac/rgu/rectv/MainActivity.java
@@ -2,13 +2,34 @@ package uk.ac.rgu.rectv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the Continue button
+        Button btnContinue = findViewById(R.id.btnContinue);
+
+        // set the click listener to the btnContinue Button
+        btnContinue.setOnClickListener(this);
+    }
+
+    // Changing Activity
+    @Override
+    public void onClick(View view) {
+        // view is the View (Button, ExitText, TextView, etc) that was clicked
+        if (view.getId() == R.id.btnContinue) {
+            // create an intent
+            Intent intent = new Intent(getApplicationContext(), RecommendationsActivity.class);
+            // start the Activity
+            startActivity(intent);
+        }
     }
 }
