0b31674af26bab1efadf43b9fa49f0c48ad602b4
louisefindlay23 <louisefindlay23@outlook.com>
2019-11-15 15:36:45 +0000

    Data is grabbed from Trakt API and stored in a Text View. Currently only show details and for Game of Thrones only (only one that works). Error message needs to be stored in a string

--- app/src/main/java/uk/ac/rgu/rectv/RecommendationsActivity.java
+++ app/src/main/java/uk/ac/rgu/rectv/RecommendationsActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Set;
 
 public class RecommendationsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // TAG to be used when logging
+    private static final String TAG = RecommendationsActivity.class.getCanonicalName();
+
+    // constant for downloading show data
+    private static final String SHOW_URL_TEMPLATE = "https://private-anon-b6a956c00a-trakt.apiary-mock.com/shows/%s";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +43,46 @@ public class RecommendationsActivity extends AppCompatActivity implements View.O
 
         // set the click listener to the PersonofInterest Poster image
         ivPersonofInterestPoster.setOnClickListener(this);
+
+        // Download show name from Trakt API
+        downloadShowNames();
+    }
+
+    public void downloadShowNames(){
+        // Downloads and displays a show name from Trakt API
+        String getShowDetailsForShowNames = (getString(R.string.personofinterest_name));
+
+        Log.d(TAG, "getting the show details for" + getShowDetailsForShowNames);
+
+        // if there's no show name to download details for then exit
+        if (getShowDetailsForShowNames == null) {
+            return;
+        }
+
+        // build string for the URL to get the show details from
+        String url = String.format(SHOW_URL_TEMPLATE, getShowDetailsForShowNames);
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        TextView tvShowNameDisplay = findViewById(R.id.tvShowNameDisplay);
+                        tvShowNameDisplay.setText(response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                TextView tvShowNameDisplay = findViewById(R.id.tvShowNameDisplay);
+
+                // NEED TO ADD THIS AS A STRING
+                tvShowNameDisplay.setText("There was an error" + error.getLocalizedMessage());
+            }
+        });
+
+        // make the request to download the show details
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        queue.add(stringRequest);
     }
 
     // Changing Activity
